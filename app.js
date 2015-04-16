d5771b40fe0dcb78071c6bd176b46cfac782daba
Harry Chen <harrychenca@gmail.com>
2015-04-16 23:09:32 +0800

    feat(bower): Add Bower and Bootstrap

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require("path");
+
+app.use('/public',  express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  // res.sendFile('test.html');
+  res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('CodeForX listening at http://$s:$s', host, port);
+  console.log('CodeForX listening at http://%s:%s', host, port);
 });
