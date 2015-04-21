9617ff1c0e957c701e66ba68d11b9dcbeeb5d588
Harry Chen <harrychenca@gmail.com>
2015-04-21 22:31:21 +0800

    feat(database): Add MongoDB through Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var path = require("path");
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/solveforx');
+
+// MongoDB Schema
+var answerSchema = mongoose.Schema({
+  content: String
+});
+
+var Answer = mongoose.model('Answer', answerSchema);
 
 app.use('/public',  express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
@@ -16,15 +26,23 @@ app.get('/', function(req, res){
 app.post('/check', function(req, res){
   if (!req.body) return res.sendStatus(400)
 
-  var functionName = "test";
+  var answer = new Answer({ content: req.body.answer });
 
-  var answerEval = eval(req.body.answer + functionName + "(9)");
+  answer.save(function (err, answer) {
+    if (err) return console.error(err);
 
-  if (answerEval === 9) {
-    res.send("Correct");
-  } else {
-    res.send("Incorrect");
-  }
+    var functionName = "test";
+
+    var answerEval = eval(answer.content + functionName + "(9)");
+
+    if (answerEval === 9) {
+      res.send("Correct");
+    } else {
+      res.send("Incorrect");
+    }
+
+    console.log(answer);
+  });
 });
 
 var server = app.listen(process.env.PORT || 3000, function(){
